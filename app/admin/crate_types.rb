bfbb641ba50efe2f479d8cf93a175e3e933bd22f
Dale Salangoste <dalesalangoste@gmail.com>
2025-04-16 21:00:00 -0500

    Add category management system with CrateType associations

--- app/admin/crate_types.rb
+++ app/admin/crate_types.rb
@@ -1,12 +1,14 @@
 ActiveAdmin.register CrateType do
-  permit_params :name, :description, :price
+  permit_params :name, :description, :price, category_ids: []
 
   index do
     selectable_column
     id_column
     column :name
-    column :price do |crate|
-      number_to_currency(crate.price)
+    column :description
+    column :price
+    column "Categories" do |crate_type|
+      crate_type.categories.map(&:name).join(", ")
     end
     column :created_at
     actions
@@ -21,6 +23,8 @@ ActiveAdmin.register CrateType do
       f.input :name
       f.input :description
       f.input :price, step: :any
+      f.input :category_ids, as: :select, collection: Category.all,
+              input_html: { multiple: true }, label: "Categories"
     end
     f.actions
   end
@@ -30,8 +34,9 @@ ActiveAdmin.register CrateType do
       row :id
       row :name
       row :description
-      row :price do |crate|
-        number_to_currency(crate.price)
+      row :price
+      row "Categories" do |crate_type|
+        crate_type.categories.map(&:name).join(", ")
       end
       row :created_at
       row :updated_at
